49af6efb973fdec4ae68105f5da7230973f38d6c
Vitaliy Roy <vitaliybrandet@gmail.com>
2023-06-18 21:42:42 +0300

    Добавьте файлы проекта.

---
+++ kr/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace kr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.clock_PictureBox = new System.Windows.Forms.PictureBox();
+            this.SetAlarmButton = new System.Windows.Forms.Button();
+            this.minuteTextBox = new System.Windows.Forms.TextBox();
+            this.hourTextBox = new System.Windows.Forms.TextBox();
+            this.TurnOffAlarmButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.image1Button = new System.Windows.Forms.Button();
+            this.image3Button = new System.Windows.Forms.Button();
+            this.image2Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.clock_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clock_PictureBox
+            // 
+            this.clock_PictureBox.ErrorImage = null;
+            this.clock_PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("clock_PictureBox.Image")));
+            this.clock_PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.clock_PictureBox.Name = "clock_PictureBox";
+            this.clock_PictureBox.Size = new System.Drawing.Size(500, 500);
+            this.clock_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.clock_PictureBox.TabIndex = 0;
+            this.clock_PictureBox.TabStop = false;
+            this.clock_PictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.clock_PictureBox_MouseDown);
+            // 
+            // SetAlarmButton
+            // 
+            this.SetAlarmButton.Location = new System.Drawing.Point(12, 525);
+            this.SetAlarmButton.Name = "SetAlarmButton";
+            this.SetAlarmButton.Size = new System.Drawing.Size(130, 23);
+            this.SetAlarmButton.TabIndex = 1;
+            this.SetAlarmButton.Text = "Встановити будильник";
+            this.SetAlarmButton.UseVisualStyleBackColor = true;
+            this.SetAlarmButton.Click += new System.EventHandler(this.SetAlarmButton_Click);
+            // 
+            // minuteTextBox
+            // 
+            this.minuteTextBox.Location = new System.Drawing.Point(148, 555);
+            this.minuteTextBox.Name = "minuteTextBox";
+            this.minuteTextBox.Size = new System.Drawing.Size(64, 20);
+            this.minuteTextBox.TabIndex = 2;
+            // 
+            // hourTextBox
+            // 
+            this.hourTextBox.Location = new System.Drawing.Point(148, 525);
+            this.hourTextBox.Name = "hourTextBox";
+            this.hourTextBox.Size = new System.Drawing.Size(64, 20);
+            this.hourTextBox.TabIndex = 3;
+            // 
+            // TurnOffAlarmButton
+            // 
+            this.TurnOffAlarmButton.Location = new System.Drawing.Point(12, 552);
+            this.TurnOffAlarmButton.Name = "TurnOffAlarmButton";
+            this.TurnOffAlarmButton.Size = new System.Drawing.Size(130, 23);
+            this.TurnOffAlarmButton.TabIndex = 4;
+            this.TurnOffAlarmButton.Text = "Вимкнути будильник";
+            this.TurnOffAlarmButton.UseVisualStyleBackColor = true;
+            this.TurnOffAlarmButton.Click += new System.EventHandler(this.TurnOffAlarmButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 528);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Години";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 557);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Хвилини";
+            // 
+            // image1Button
+            // 
+            this.image1Button.Location = new System.Drawing.Point(286, 525);
+            this.image1Button.Name = "image1Button";
+            this.image1Button.Size = new System.Drawing.Size(102, 23);
+            this.image1Button.TabIndex = 8;
+            this.image1Button.Text = "Циферблат №1";
+            this.image1Button.UseVisualStyleBackColor = true;
+            this.image1Button.Click += new System.EventHandler(this.image1Button_Click);
+            // 
+            // image3Button
+            // 
+            this.image3Button.Location = new System.Drawing.Point(394, 525);
+            this.image3Button.Name = "image3Button";
+            this.image3Button.Size = new System.Drawing.Size(95, 23);
+            this.image3Button.TabIndex = 9;
+            this.image3Button.Text = "Цифербалт №3";
+            this.image3Button.UseVisualStyleBackColor = true;
+            this.image3Button.Click += new System.EventHandler(this.image3Button_Click);
+            // 
+            // image2Button
+            // 
+            this.image2Button.Location = new System.Drawing.Point(286, 552);
+            this.image2Button.Name = "image2Button";
+            this.image2Button.Size = new System.Drawing.Size(102, 23);
+            this.image2Button.TabIndex = 10;
+            this.image2Button.Text = "Циферблат №2";
+            this.image2Button.UseVisualStyleBackColor = true;
+            this.image2Button.Click += new System.EventHandler(this.image2Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 587);
+            this.Controls.Add(this.image2Button);
+            this.Controls.Add(this.image3Button);
+            this.Controls.Add(this.image1Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TurnOffAlarmButton);
+            this.Controls.Add(this.hourTextBox);
+            this.Controls.Add(this.minuteTextBox);
+            this.Controls.Add(this.SetAlarmButton);
+            this.Controls.Add(this.clock_PictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.clock_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox clock_PictureBox;
+        private System.Windows.Forms.Button SetAlarmButton;
+        private System.Windows.Forms.TextBox minuteTextBox;
+        private System.Windows.Forms.TextBox hourTextBox;
+        private System.Windows.Forms.Button TurnOffAlarmButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button image1Button;
+        private System.Windows.Forms.Button image3Button;
+        private System.Windows.Forms.Button image2Button;
+    }
+}
+
---
+++ kr/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace kr
+{
+    public partial class Form1 : Form
+    {
+        // Підключення функцій з бібліотеки user32.dll, що допоможуть переміщувати вікно мишкою
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        internal extern static bool PostMessage(IntPtr hWnd, uint Msg, uint WParam, uint LParam);
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        internal extern static bool ReleaseCapture();
+
+        // Положення та розміри стрілок
+        int center_x = 250, center_y = 250;
+        int second_hand_size = 140, minute_hand_size = 130, hour_hand_size = 115;
+
+        private bool alarmSet = false; // Змінна для відстеження статусу будильника
+        private int alarmHour; // Змінна для збереження години будильника
+        private int alarmMinute; // Змінна для збереження хвилини будильника
+
+        // Налаштування таймера для запуску функції t_Tick кожної секунди
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.main);
+            t.Start();
+        }
+
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Функція, що дозволяє при натисканні на PictureBox переміщувати вікно програми
+        private void clock_PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            const uint WM_SYSCOMMAND = 0x0112;
+            const uint DOMOVE = 0xF012;
+
+            ReleaseCapture();
+            PostMessage(this.Handle, WM_SYSCOMMAND, DOMOVE, 0);
+        }
+
+        // Функція для обрахунку координат секундної та хвилинної стрілок
+        private int[] second_and_minute_coordinate(int value, int hands_lenght)
+        {
+            int[] coord = new int[2];
+            value *= 6;
+
+            if (value >= 0 && value <= 180)
+            {
+                coord[0] = center_x + (int)(hands_lenght * Math.Sin(Math.PI * value / 180));
+                coord[1] = center_y - (int)(hands_lenght * Math.Cos(Math.PI * value / 180));
+            }
+            else
+            {
+                coord[0] = center_x - (int)(hands_lenght * -Math.Sin(Math.PI * value / 180));
+                coord[1] = center_y - (int)(hands_lenght * Math.Cos(Math.PI * value / 180));
+            }
+            return coord;
+        }
+
+        // Функція обрахунк координат годинної стрілки
+        private int[] hour_coordinate(int hour_value, int minute_value, int hand_lenght)
+        {
+            int[] coord = new int[2];
+            int val = (int)((hour_value * 30) + (minute_value * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = center_x + (int)(hand_lenght * Math.Sin(Math.PI * val / 180));
+                coord[1] = center_y - (int)(hand_lenght * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = center_x - (int)(hand_lenght * -Math.Sin(Math.PI * val / 180));
+                coord[1] = center_y - (int)(hand_lenght * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        // Основна функція
+        private void main(object sender, EventArgs e)
+        {
+            // Отримуємо системний час
+            int second = DateTime.Now.Second;
+            int minute = DateTime.Now.Minute;
+            int hour = DateTime.Now.Hour;
+
+            int[] handCoord = new int[2];
+
+            Graphics f = clock_PictureBox.CreateGraphics();
+
+            this.Text = "Аналоговий годинник " + hour + ":" + minute + ":" + second;
+
+            // Стираємо попереднє положення стрілок годинника
+            handCoord = second_and_minute_coordinate(second, second_hand_size + 4);
+            f.DrawLine(new Pen(Color.White, 45f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            handCoord = second_and_minute_coordinate(minute, minute_hand_size + 4);
+            f.DrawLine(new Pen(Color.White, 40f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            handCoord = hour_coordinate(hour % 12, minute, hour_hand_size + 4);
+            f.DrawLine(new Pen(Color.White, 20f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            // Малюємо стрілки годинника
+            handCoord = hour_coordinate(hour % 12, minute, hour_hand_size);
+            f.DrawLine(new Pen(Color.Black, 6f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            handCoord = second_and_minute_coordinate(minute, minute_hand_size);
+            f.DrawLine(new Pen(Color.Red, 5f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            handCoord = second_and_minute_coordinate(second, second_hand_size);
+            f.DrawLine(new Pen(Color.Blue, 4f), new Point(center_x, center_y), new Point(handCoord[0], handCoord[1]));
+
+            // Відображення статусу будильника
+            if (alarmSet)
+            {
+                if (hour == alarmHour && minute == alarmMinute)
+                {
+                    // Спрацювання будильника
+                    MessageBox.Show("Будильник спрацював!");
+                    alarmSet = false;
+                }
+                else
+                {
+                    this.Text += " (Будильник заведено)";
+                }
+            }
+            else
+            {
+                this.Text += " (Будильник не заведено)";
+            }
+        }
+
+        // Функція для встановлення будильника з перевіркою введеного часу
+        private void SetAlarm(int hour, int minute)
+        {
+            // Перевірка введеного часу
+            if (hour < 0 || hour > 23)
+            {
+                MessageBox.Show("Некоректно введена година!");
+                return;
+            }
+
+            if (minute < 0 || minute > 59)
+            {
+                MessageBox.Show("Некоректно введена хвилина!");
+                return;
+            }
+
+            alarmHour = hour;
+            alarmMinute = minute;
+            alarmSet = true;
+            this.Text += " (Будильник заведено)";
+        }
+
+        // Обробник події натискання кнопки "Завести будильник"
+        private void SetAlarmButton_Click(object sender, EventArgs e)
+        {
+            int hour, minute;
+
+            // Перевірка коректності введення години
+            if (!int.TryParse(hourTextBox.Text, out hour))
+            {
+                MessageBox.Show("Некоректне годинне значення!");
+                return;
+            }
+
+            // Перевірка коректності введення хвилини
+            if (!int.TryParse(minuteTextBox.Text, out minute))
+            {
+                MessageBox.Show("Некоректне хвилинне значення!");
+                return;
+            }
+
+            SetAlarm(hour, minute);
+        }
+
+        // Обробник події натискання кнопки "Вимкнути будильник"
+        private void TurnOffAlarmButton_Click(object sender, EventArgs e)
+        {
+            alarmSet = false;
+        }
+
+        // Функція для зміни зображення у PictureBox
+        private void ChangeImage(string imagePath)
+        {
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                try
+                {
+                    Image image = Image.FromFile(imagePath);
+                    clock_PictureBox.Image = image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при завантаженні зображення: " + ex.Message);
+                }
+            }
+        }
+
+        // Перший варіант циферблату для годинника
+        private void image1Button_Click(object sender, EventArgs e)
+        {
+            string imagePath = "Images/1.gif"; 
+            ChangeImage(imagePath);
+        }
+
+        // Другий варіант циферблату для годинника
+        private void image2Button_Click(object sender, EventArgs e)
+        {
+            string imagePath = "Images/2.jpg";
+            ChangeImage(imagePath);
+        }
+
+        // Третій варіант циферблату для годинника
+        private void image3Button_Click(object sender, EventArgs e)
+        {
+            string imagePath = "Images/3.png";
+            ChangeImage(imagePath);
+        }
+    }
+}
